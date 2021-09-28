68a8bebbef5ded694e8f4e6f7b16cb83b55941c9
morioka1206 <r.morioka@icloud.com>
2021-09-28 16:49:54 +0900

    Revert "culcul作成"
    This reverts commit 987d3968d1077542a8b774f480e58d6751f3945e.

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" width="100px" src="https://2.bp.blogspot.com/--BYLyPduBgM/WaPvldUDt9I/AAAAAAABGNw/XTkwA_fBEC86bpvy2z7-1jZ9-dEc_6_jQCLcBGAs/s800/dentaku_syoumen_small.png
-">
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <calculator />
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
+
 <script>
-import Calculator from './components/Calculator.vue'
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    Calculator
+    HelloWorld
   }
 }
 </script>
