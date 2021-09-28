37c1e59ec8db896f6eb2061e0f5e7e25e5ba80ef
morioka1206 <r.morioka@icloud.com>
2021-09-28 18:10:15 +0900

    Revert "Merge branch 'master' of https://github.com/morioka1206/culcul"
    This reverts commit b37d84c479e226349267bf0da5f13b80dfef988b, reversing
    changes made to 10d231f2179e24b99c56aea906c108a4f45b686f.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" width="100px" src="https://2.bp.blogspot.com/--BYLyPduBgM/WaPvldUDt9I/AAAAAAABGNw/XTkwA_fBEC86bpvy2z7-1jZ9-dEc_6_jQCLcBGAs/s800/dentaku_syoumen_small.png
+">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <calculator />
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Calculator from './components/Calculator.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Calculator
   }
 }
 </script>
